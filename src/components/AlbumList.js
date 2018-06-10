d5800374379817e13ccc12dde5497fd98a8380f8
SAU-Pongsakorn21 <s5652410021@sau.ac.th>
2018-06-10 20:12:25 +0700

    lecture 33-42

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetails from './AlbumDetails';
 
 class AlbumList extends Component {
-
+    state = { albums: [] } ;
     componentWillMount() {
-        console.log('componentWillMount in AlbumList');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => this.setState({
+            albums : response.data
+        }));
+    }
+    
+    renderAlbums() {
+        return this.state.albums.map( album => 
+            <AlbumDetails key={album.title} album={album} />
+        );
     }
 
     render() {
+        console.log(this.state);
         return (
             <View>
-                <Text>Albums List !!!</Text>
+                {this.renderAlbums() }
             </View>
         );
     }
